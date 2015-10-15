855d65ef1eeb51136d8bfe86f8a421c66343f634
Jeff Capuano <jcapuano328@gmail.com>
2015-10-15 06:54:57 -0400

    try a drawer

--- MainView.js
+++ MainView.js
@@ -1,15 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
-var {AppRegistry,} = React;
 var { View, Text, Image, StyleSheet } = React;
 var DrawerLayout = require("./DrawerLayout");
 
-var lb = React.createClass({
+var MainView = React.createClass({
 
   getInitialState() {
     return {};
@@ -52,54 +47,4 @@ var styles = StyleSheet.create({
   },
 });
 
-
-/*
-var lb = require('./MainView');
-*/
-/*
-var {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} = React;
-
-var lb = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-});
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-*/
-
-AppRegistry.registerComponent('lb', () => lb);
+module.exports = MainView;
