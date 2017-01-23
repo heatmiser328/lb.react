29f34583cad82e464b979b4505d2ec2a143e9fd9
Jeff Capuano <jcapuano328@gmail.com>
2017-01-23 07:21:13 -0500

    back button for about view

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,12 +23,20 @@ const navBarOpts = {
     rightButtons: rightButtons
 };
 
+const navBarOptsAbout = {
+    ...navBarOpts,
+    left: 'back',
+    onBack: Actions.pop
+};
+
+
 const NavBarMain = NavBar(navBarOpts);
+const NavBarAbout = NavBar(navBarOptsAbout);
 
 export default Actions.create(
     <Scene key="root" navBar={NavBarMain}>
         <Scene key="home" type="reset" component={HomeView} title="La Bataille Assistant" initial={true}/>
         <Scene key="battle" component={BattleView} title="" />
-        <Scene key="about" component={AboutView} title="About" />
+        <Scene key="about" navBar={NavBarAbout} component={AboutView} title="About" />
     </Scene>
 );
