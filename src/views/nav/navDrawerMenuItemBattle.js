4099f2e7470201849b2cb4416b9285acd14ea8ae
Jeff Capuano <jcapuano328@gmail.com>
2017-03-18 10:28:33 -0400

    fix the collapsible navigation bug

--- src/views/nav/navDrawerMenuItemBattle.js
+++ src/views/nav/navDrawerMenuItemBattle.js
@@ -13,44 +13,40 @@ var BattleNavMenuItem = React.createClass({
     render() {
         let battle = this.props.battle || this.props.item || {};
         return (
-            <TouchableOpacity onPress={this.onPress}>
-                <View style={{flex: 1,backgroundColor: '#fff'}}>
-                    <View style={{
-                        alignItems: 'flex-start',
-                        justifyContent: 'flex-start',
-                        flex: 1,
-                        flexDirection: 'row',
-                        margin: 5,
-                        padding: 5,
-                        backgroundColor: '#eaeaea',
-                        borderRadius: 3
-                    }}>
-                        {/*
-                        <View style={{marginTop: 40, marginRight: 5}}>
-                            <Arrow size={18} direction={this.state.expanded ? 'down' : 'right'} />
-                        </View>
-                        */}
-                        <Image style={{
-                            //flex: 1,
-                            //width: null,
-                            //height: null,
-                            width: 64,
-                            height: 96,
-                            resizeMode: 'contain',
-                            //backgroundColor: 'transparent',
-                        }} source={this.props.icons[battle.image]} />
-                        <View style={{flex: 1}}>
-                            <Text style={{fontSize: Style.Font.large(),textAlign: 'center',margin: 10}}>{battle.name}</Text>
-                            <Text style={{fontSize: Style.Font.medium(),textAlign: 'center',margin: 10,color: 'blue'}}>{battle.publisher}</Text>
-                        </View>
-                    </View>
-                    {this.renderScenarios(battle)}
+            <View style={{flex: 1,backgroundColor: '#fff'}}>            
+                <View style={{flex:1}}>
+                    <TouchableOpacity onPress={this.onPress}>                
+                        <View style={{
+                            alignItems: 'flex-start',
+                            justifyContent: 'flex-start',
+                            flex: 1,
+                            flexDirection: 'row',
+                            margin: 5,
+                            padding: 5,
+                            backgroundColor: '#eaeaea',
+                            borderRadius: 3
+                        }}>
+                            <View style={{marginTop: 40, marginRight: 5}}>
+                                <Arrow size={18} direction={this.state.expanded ? 'down' : 'right'} />
+                            </View>
+                            <Image style={{
+                                width: 64,
+                                height: 96,
+                                resizeMode: 'contain',
+                            }} source={this.props.icons[battle.image]} />
+                            <View style={{flex: 1}}>
+                                <Text style={{fontSize: Style.Font.large(),textAlign: 'center',margin: 10}}>{battle.name}</Text>
+                                <Text style={{fontSize: Style.Font.medium(),textAlign: 'center',margin: 10,color: 'blue'}}>{battle.publisher}</Text>
+                            </View>
+                        </View>                                        
+                    </TouchableOpacity>
                 </View>
-            </TouchableOpacity>
+                {this.renderScenarios(battle)}
+            </View>
         );
     },
     renderScenarios(battle) {
-        if (true) {//this.state.expanded) {
+        if (this.state.expanded) {
             return (
                 <View>
                     {battle.scenarios.map((scenario,i) => {
@@ -66,7 +62,7 @@ var BattleNavMenuItem = React.createClass({
             );
 
         }
-        return <Text/>;                    
+        return null;                    
     }
 });
 
