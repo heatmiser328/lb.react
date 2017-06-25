650bb0e2540b9993ccec944cf9698c0902a69482
Jeff Capuano <jcapuano328@gmail.com>
2017-06-25 09:10:31 -0400

    introduce publisher into navigation hierarchy

--- src/services/battles.js
+++ src/services/battles.js
@@ -26,6 +26,41 @@ var rules = {
 
 module.exports = {
     battles: battles,
+    battlesByPublisher() {
+        /* [
+            {
+                name: string,
+                image: string,
+                battles: array
+            }
+        ]        
+        */
+        return battles.reduce((a, battle) => {
+            console.log('battlesByPublisher', a);
+            if (!a.find((p) => p.name == battle.publisher)) {
+                a.push({
+                    name: battle.publisher, 
+                    image: battle.publisher,
+                    battles: battles.filter((b) => b.publisher == battle.publisher)
+                });
+            }
+            return a;
+        }, []).sort((l,r) => {
+            const pubIndex = (pub) => {
+                pub = pub.toLowerCase();
+                if (pub == 'marshal enterprises') {
+                    return 0;
+                } else if (pub == 'clash of arms') {
+                    return 1;
+                }
+                return 2;
+            }            
+            const diff = pubIndex(l.name) - pubIndex(r.name);
+            if (diff < 0) { return -1; }
+            if (diff > 0) { return 1; }
+            return 0;
+        });
+    },
     battle(battleid) {
         return battles.find((b,i) => b.id == battleid) || {scenarios:[]};
     },
