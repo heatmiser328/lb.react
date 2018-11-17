99eea3bf0ea320c2b577aaeefa15c29d92803345
Jeff Capuano <jcapuano328@gmail.com>
2018-11-17 09:20:52 -0500

    fix

--- src/reducers/info.js
+++ src/reducers/info.js
@@ -1,6 +1,6 @@
 let defaultInfo = {
     version: '1.8.16',
-    releasedate: new Date(2018,10,17,9,09,0)//new Date()
+    releasedate: new Date(2018,10,17,9,18,0)//new Date()
 };
 
 module.exports = (state = defaultInfo, action) => {
