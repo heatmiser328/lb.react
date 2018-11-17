220f742346a5a53e76a060ff73602f79efb872b0
Jeff Capuano <jcapuano328@gmail.com>
2018-11-17 09:10:40 -0500

    melee mod update

--- src/reducers/info.js
+++ src/reducers/info.js
@@ -1,6 +1,6 @@
 let defaultInfo = {
-    version: '1.8.15',
-    releasedate: new Date(2018,10,11,11,38,0)//new Date()
+    version: '1.8.16',
+    releasedate: new Date(2018,10,17,9,09,0)//new Date()
 };
 
 module.exports = (state = defaultInfo, action) => {
