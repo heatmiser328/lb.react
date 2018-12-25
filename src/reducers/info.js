edba96ccdfdc711f49b0ca8aa9ce8dff20c3da87
Jeff Capuano <jcapuano328@gmail.com>
2018-12-25 09:44:37 -0500

    wagram update

--- src/reducers/info.js
+++ src/reducers/info.js
@@ -1,6 +1,6 @@
 let defaultInfo = {
-    version: '1.8.16',
-    releasedate: new Date(2018,10,17,9,18,0)//new Date()
+    version: '1.8.19',
+    releasedate: new Date(2018,11,25,9,28,0)//new Date()
 };
 
 module.exports = (state = defaultInfo, action) => {
